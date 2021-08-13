d2c02d26a0bdbcf8dd7cb40ac0ce29c3cd1d6444
Mike G <gijon.internet@gmail.com>
2021-08-13 23:15:50 +0200

    polimorfismo

--- ClasesYPolimorfismo.py
+++ ClasesYPolimorfismo.py
@@ -8,11 +8,46 @@ class Vehiculo:
         return (f" Marca: {self.marca}, Motor: {self.motor}, Ruedas: {self.ruedas} desde __str")
     def __descripcion__(self):
         return (f" Marca: {self.marca}, Motor: {self.motor}, Ruedas: {self.ruedas} desde __descripcion__")
-    def __descripcion(self):
+    def descripcion(self):
         return (f" Marca: {self.marca}, Motor: {self.motor}, Ruedas: {self.ruedas} desde descripcion")
 
 vehi= Vehiculo("Kia ceed",4,"diesel")
 print(str(vehi))
 print(vehi.__str__())
 print((vehi.__descripcion__()))
-print(vehi.__descripcion()) # este no se va a ver porque empieza por __ y no acaba por __
+print(vehi.descripcion()) # este no se va a ver porque empieza por __ y no acaba por __
+
+## Ejemplo de polimorfismo
+
+class Moto(Vehiculo):
+    def __init__(self,marca,ruedas,motor, cilindrada):
+        super().__init__(marca,ruedas,motor)
+        self.cilindrada = cilindrada
+        
+    def descripcion(self):
+        return (f" Marca: {self.marca}, Motor: {self.motor}, Ruedas: {self.ruedas}, cilindrada:{self.cilindrada}  desde descripcion")
+
+class Bicicleta(Vehiculo):
+    def __init__(self,marca,ruedas,motor, peso):
+       super().__init__(marca,ruedas,motor)
+       self.peso = peso
+    def descripcion(self):
+        return (f" Marca: {self.marca}, Motor: {self.motor}, Ruedas: {self.ruedas}, peso:{self.peso}  desde descripcion")
+
+
+
+
+moto = Moto("yamaha",2,"Gasolina","500cc")
+print(moto.descripcion())
+
+bici = Bicicleta("BH",2,"piernas","700gr")
+print(bici.descripcion())
+
+def polimorfismo(auto):
+    print(auto.descripcion())
+
+polimorfismo(moto)
+polimorfismo(bici)
+
+## Aqui se ve como se hace el polimorfismo, en la funcion polimorfismo se le mete una moto o una bici y llamamos a su descripcion porque su clase
+## base también tiene el metodo descripcion pero sabe llamar a la implementación adecuada de cada objeto.
