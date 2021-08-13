16bb7cb6d634e61971cb2a200d0e0ba40967beda
Mike G <gijon.internet@gmail.com>
2021-08-13 21:13:46 +0200

    Primeros ejemplos python

--- PrimeraPython.py
+++ PrimeraPython.py
@@ -1,20 +1,59 @@
-texto = "en un lugar de la mancha de cuyo nombre no puedo acordarme hace mucho que vivia a a a b b b c c"
+# """ texto = "en un lugar de la mancha de cuyo nombre no puedo acordarme hace mucho que vivia a a a b b b c c"
 
-texto_dividido= texto.split()
+# texto_dividido= texto.split()
 
-d= {'a':3}
+# d= {'a':3}
 
-#d = {'uno': 1, 'dos': 2, 'tres': 3}
-#print(d.get('a'))
-for palabra in texto_dividido:
-    print (palabra)
-    contador = d.get(palabra)
-    if contador is not None:
-        d[palabra] = contador + 1
-    else:
-        d[palabra] =  1
+# #d = {'uno': 1, 'dos': 2, 'tres': 3}
+# #print(d.get('a'))
+# for palabra in texto_dividido:
+#     print (palabra)
+#     contador = d.get(palabra)
+#     if contador is not None:
+#         d[palabra] = contador + 1
+#     else:
+#         d[palabra] =  1
 
 
-print (d)
+# print (d)
 
-print (d.keys)
+# print (d.keys) """
+#Estudiar por que loa trucos
+
+# """ class Operaciones:
+#     texto = ""
+#     tricks = []
+#     def __init__(self, name):
+#         self.texto = name
+#         #self.tricks = []    # creates a new empty list for each dog
+      
+
+#     def add_trick(self, trick):
+#         self.tricks.append(trick)
+#     def __suma (self,para,parb):
+        
+#         suma=para+parb
+#         para=para+1
+#         return suma
+
+
+#         return a+b
+#     def getTexto(self):
+#         return self.texto
+
+
+# op =Operaciones ("hola")
+# num1=3
+# num2=4
+# op.add_trick("paso1")
+# print(f"suma {num1} mas {num2} = {op.__suma(num1,num2)} texto{op.texto} {op.getTexto()} tricks:{op.tricks} ")
+# op.texto="paso1"
+# print(f"texto:{op.texto}")
+# op2= Operaciones ("hola2")
+# op2.add_trick("paso2")
+# op2.texto="paso2"
+# num1=3
+# num2=4
+# print(f"suma {num1} mas {num2} = {op2.suma(num1,num2)} texto{op.texto} texto{op2.texto}  {op.getTexto()} tricks:{op2.tricks} ")
+
+#   """
