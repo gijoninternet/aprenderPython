931b50f1b89d03d9c2acdc8c1ab2ba3450be3a66
Mike G <gijon.internet@gmail.com>
2021-08-18 22:51:10 +0200

    git

--- variables.py
+++ variables.py
@@ -11,34 +11,55 @@
 # print("Programa terminado")
 
 
-def subrutina_ModGlobal_desdeSub():
-    #global a #  se define a como  la definida en la parte global a=3    
-    global a
-    a=1
-    print( a)
+# def subrutina_ModGlobal_desdeSub():
+#     #global a #  se define a como  la definida en la parte global a=3    
+#     global a
+#     a=1
+#     print( a)
     
-     # asignamos el valor uno a la variable 'a' que es la global ahora
-    aa = 2 
-    print(a)
-    return
+#      # asignamos el valor uno a la variable 'a' que es la global ahora
+#     aa = 2 
+#     print(a)
+#     return
 
-print ("las sub solo alteran globales si se define como global en la sub")
-a = 3
-subrutina_ModGlobal_desdeSub()
+# print ("las sub solo alteran globales si se define como global en la sub")
+# a = 3
+# subrutina_ModGlobal_desdeSub()
 
-print(a)
+# print(a)
 
-def subrutina_lasSubNoAlteranglobales():
-    #global a #  se define a como  la definida en la parte global a=3    
-    a=1
-    print( a)
+# def subrutina_lasSubNoAlteranglobales():
+#     #global a #  se define a como  la definida en la parte global a=3    
+#     a=1
+#     print( a)
     
-     # asignamos el valor uno a la variable 'a' que es la global ahora
-    aa = 2 
-    print(a)
-    return
-print ("las sub no alteran globales aunqur sean  mismo valor")
-a = 3
-subrutina_lasSubNoAlteranglobales()
+#      # asignamos el valor uno a la variable 'a' que es la global ahora
+#     aa = 2 
+#     print(a)
+#     return
+# print ("las sub no alteran globales aunqur sean  mismo valor")
+# a = 3
+# subrutina_lasSubNoAlteranglobales()
 
-print(a)
+# print(a)
+
+
+def doblar_valores(numeros):
+    pos = 0
+    for valor in numeros:
+        print (numeros[pos])
+        numeros[pos] = numeros[pos]*3
+        pos = pos+ 1
+    print("dentro de doblar valores")
+    print (numeros)
+    # for i,n in enumerate(numeros):
+    #     print("valores de i,n ",i,n)
+    #     numeros[i] *= 2
+
+ns = [10,50,100]
+doblar_valores(ns.copy())
+#doblar_valores(ns[:])  # Una copia al vuelo de una lista con [:]
+
+print(ns)
+doblar_valores(ns)  # modificamos directamente los elementos en la sub y afecta al principal también
+print(ns)
