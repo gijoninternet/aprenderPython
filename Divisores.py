9cc5178feb0b9277107bfd53cfdbaa6f72993cc5
Mike G <gijon.internet@gmail.com>
2021-08-30 01:28:03 +0200

    git

--- Divisores.py
+++ Divisores.py
@@ -1,6 +1,7 @@
 lista_primos_menos= [2,3,5,7,11,13,17,19,23]
-lista_primos=[2,3,5]
-numero=121
+lista_primos=[]
+numero_inicial=403
+numero=numero_inicial
 dic_divisores={1:1}
 
 
@@ -15,10 +16,12 @@ def divisores_primos(numero):
 
 def divisores_dividiendo(numero):
     primo_encontrado = None
-    for primo_prueba in range(2, int(numero/2)):
+    #se prueba con el 2 y numeros impares
+    lista_numeros_probar=[2] + list(range(3, int(numero/2 +1),2))
+    for primo_prueba in lista_numeros_probar:
         if numero%primo_prueba == 0:
             primo_encontrado=primo_prueba
-
+            break
     return primo_encontrado
    
 
@@ -34,19 +37,29 @@ while (primo is not None):
         
     else:
        dic_divisores[primo] = 1 
-    numero=numero/primo
+    numero=int(numero/primo)
     primo=divisores_primos(numero)
 else:
     print("buscar primos dividiendo hasta numero/2 de ", numero )
     primo=divisores_dividiendo(numero)
+    print("divisores_dividiendo de,", numero,primo)
     while (primo is not None):
         contador = dic_divisores.get(primo)
         if contador is not None:
             dic_divisores[primo] = contador + 1    
         else:
             dic_divisores[primo] = 1 
-        numero=numero/primo
+        numero=int(numero/primo)
+        print("tipo",type(numero))
         primo=divisores_dividiendo(numero)
+    else:
+        #no ha encontrado divisor pero el numero existe
+        if numero!=numero_inicial:
+            contador = dic_divisores.get(numero)
+            if contador is not None:
+                dic_divisores[numero] = contador + 1    
+            else:
+                dic_divisores[numero] = 1 
 
     
 
---
+++ test.py
@@ -0,0 +1,9 @@
+numero=20
+lista=[2] + list(range(3, int(numero/2 +1),2))
+#lista2= range(2) + lista
+
+print(lista)
+#listados= list(lista)
+#print(lista2)
+for primo_prueba in lista:
+    print(primo_prueba)
